f9d38dd5f4e2a36e49e29199b70c0b59a3956edb
SunnyRx <sunny558@qq.com>
2016-05-25 14:10:42 +0800

    添加对话框

--- README.md
+++ README.md
@@ -8,3 +8,23 @@
 其中只对`index.html`, `main.js`, `style.css`进行修改，请阅读这三个文件内的注释
 
 每次对文件的修改只允许**1**个人进行，当合并到`master`后下一人需先`pull`后进行接龙
+
+————————————————————————————————————————————————————————
+2016/05：第三次接龙
+接龙人：热心
+修改：
+1、装备和背包框的文字大小从5px改到10px。
+2、重新整理了style.css样式
+
+添加：
+1、对话框Dialog，主要有一个变量和四个方法
+	变量：
+	currentDialogTextIndex：记录当前展示文字的序号
+	方法：
+	showDialog()：显示对话框
+	hideDialog()：隐藏对话框
+	setDialogText(text)：设置对话框内的文字
+	playDialog()：展示下一句对话框文字，通常设置为dialog的onclick事件
+
+	在main.js中，使用了dialogText数组储存文字并测试输出。
+	PS：实际上我只成功测试了playDialog()的作用，在我浏览器中并没有成功激活onclick事件_(:3」∠)_
--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ equip = equipCanvas.getContext('2d');
 // 创建背包栏的Context对象
 var bagCanvas = document.getElementById("bag");
 bag = bagCanvas.getContext('2d');
+// 获取对话框对象
+var dialog = document.getElementById("dialog");
+
+
+var currentDialogTextIndex = 0;	//记录当前显示的对话序号，开始播放前设置为0
+// 测试数据
+var dialogText=new Array()
+	dialogText[0]='人族以及魔族,早在三百年前,就開始了戰爭。';
+	dialogText[1]='戰爭帶來的損失以及災害,令百姓生活苦難,不論是魔族還是人族,都盼著和平。';
+	dialogText[2]='後來,人族及魔族非常奇怪出现了同一个預言:';
+	dialogText[3]='「在不久的將來,將會有一名結束戰爭的生命誕生。他擁有著紅藍異瞳,一頭紅色的頭髮。」';
+	dialogText[4]='人類貴族以及魔族貴族得知此預言後,也開始準備計劃,希望能尋獲預言之子。';
 
 // 程序入口 - 当页面加载完毕后即开始执行
 $(function() {
@@ -17,7 +29,13 @@ $(function() {
 	
 	// 绘制基础界面	
 	drawBaseUI();
-	
+	// 隐藏对话框
+	hideDialog();
+
+	//测试对话框显示台词,实际使用时应用单例模式控制
+	showDialog();
+	dialog.onclick=playDialog();
+
 });
 
 // 绘制基础界面
@@ -28,7 +46,7 @@ function drawBaseUI() {
 	equip.lineTo(60, 5);
 	equip.stroke();
 	// 绘制装备栏标题文字
-	equip.font="5px Arial";
+	equip.font="10px Arial";
 	equip.textBaseline="middle";
 	equip.fillText("EQUIPMENTS", 61, 5);
 	// 绘制装备栏标题右边线条
@@ -53,7 +71,7 @@ function drawBaseUI() {
 	bag.lineTo(110, 5);
 	bag.stroke();
 	// 绘制背包栏标题文字
-	bag.font="5px Arial";
+	bag.font="10px Arial";
 	bag.textBaseline="middle";
 	bag.fillText("BAG", 111, 5);
 	// 绘制背包栏标题右边线条
@@ -61,6 +79,7 @@ function drawBaseUI() {
 	bag.moveTo(140, 5);
 	bag.lineTo(250, 5);
 	bag.stroke();
+
 
 	// 绘制背包栏格子
 	for (var i = 0; i < 2; i++) {
@@ -70,5 +89,29 @@ function drawBaseUI() {
 			bag.rect(x + j * 50, 10 + i * 50, width, height);
 			bag.stroke();
 		}
+	}
+}
+
+//显示对话框
+function showDialog(){
+	dialog.style.display="block";
+}
+//隐藏对话框
+function hideDialog(){
+	dialog.style.display="none";
+}
+//设置对话框文字
+function setDialogText(text){
+	dialog.innerHTML=text;
+}
+//播放对话框内容
+function playDialog(){
+	//每一次点击都会显示dialogText的下一句，直到显示完后隐藏对话框
+	if (currentDialogTextIndex < dialogText.length){
+		setDialogText(dialogText[currentDialogTextIndex++]);
+	}
+	else
+	{
+		hideDialog();
 	}
 }
